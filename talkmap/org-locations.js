becabcab8c88ac07291dc67894965abddae4c05c
GVancauwenberghe <76557828+GVancauwenberghe@users.noreply.github.com>
2025-03-27 16:33:25 +0100

    Update org-locations.js

--- talkmap/org-locations.js
+++ talkmap/org-locations.js
@@ -18,5 +18,10 @@ var addressPoints = [
     "Talk 1 on Relevant Topic in Your Field<br />UC San Francisco, Department of Testing; San Francisco, CA, USA",
     37.7792588,
     -122.4193286
+  ],
+  [
+    "SEED4NA - SDI & EO Training & Education, Egypt",
+    29.308374,
+    30.844105
   ]
 ];

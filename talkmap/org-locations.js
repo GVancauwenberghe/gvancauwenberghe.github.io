eafbbd6e253b69e0cc8646cac51dfe8e9af406e7
GVancauwenberghe <76557828+GVancauwenberghe@users.noreply.github.com>
2025-03-29 15:16:37 +0100

    Update org-locations.js

--- talkmap/org-locations.js
+++ talkmap/org-locations.js
@@ -1,25 +1,5 @@
 var addressPoints = [
-  [
-    "Talk 12 on Topic in Your Field<br />London School of Testing; London, UK",
-    48.864716,
-    2.349014
-  ],
-  [
-    "Tutorial 1 on Relevant Topic in Your Field<br />UC-Berkeley Institute for Testing Science; Berkeley, CA, USA",
-    37.8708393,
-    -122.272863
-  ],
-  [
-    "Conference Proceeding talk 3 on Relevant Topic in Your Field<br />Testing Institute of America 2014 Annual Conference; Los Angeles, CA, USA",
-    34.0536909,
-    -118.242766
-  ],
-  [
-    "Talk 1 on Relevant Topic in Your Field<br />UC San Francisco, Department of Testing; San Francisco, CA, USA",
-    37.7792588,
-    -122.4193286
-  ],
-  [
+   [
     "SEED4NA - SDI & EO Training & Education for North Africa, Egypt (Alexandria & Fayoum)",
     31.241178,
     29.9589936
@@ -55,22 +35,97 @@ var addressPoints = [
     44.773674
   ],
   [
-    "SEED4NA - SDI & EO Training & Education for North Africa, Tunisia (Tunis & Jendouba)",
+    "SEED4NA - SDI & EO Training & Education for North Africa, United Kingdom (London)",
     -12.046374,
     -77.042793
   ],
   [
-    "SEED4NA - SDI & EO Training & Education for North Africa, Tunisia (Tunis & Jendouba)",
+    "SEED4NA - SDI & EO Training & Education for North Africa, the Netherlands",
     -12.046374,
     -77.042793
   ],
   [
-    "SEED4NA - SDI & EO Training & Education for North Africa, Tunisia (Tunis & Jendouba)",
+    "SEED4NA - SDI & EO Training & Education for North Africa, Italy)",
     -12.046374,
     -77.042793
   ],
   [
-    "SEED4NA - SDI & EO Training & Education for North Africa, Tunisia (Tunis & Jendouba)",
+    "SEED4NA - SDI & EO Training & Education for North Africa, Belgium (Walloon Region)",
+    -12.046374,
+    -77.042793
+  ],
+  [
+    "SEED4NA - SDI & EO Training & Education for North Africa, Belgium (Leuven))",
+    -12.046374,
+    -77.042793
+  ],
+  [
+    "SEED4NA - SDI & EO Training & Education for North Africa, Italy (Ferrara))",
+    -12.046374,
+    -77.042793
+  ],
+  [
+    "SEED4NA - SDI & EO Training & Education for North Africa, Romania)",
+    -12.046374,
+    -77.042793
+  ],
+  [
+    "SEED4NA - SDI & EO Training & Education for North Africa, Belgium)",
+    -12.046374,
+    -77.042793
+  ],
+  [
+    "SEED4NA - SDI & EO Training & Education for North Africa, Italy)",
+    -12.046374,
+    -77.042793
+  ],
+  [
+    "SEED4NA - SDI & EO Training & Education for North Africa, Vietnam (Can To))",
+    -12.046374,
+    -77.042793
+  ],
+  [
+    "SEED4NA - SDI & EO Training & Education for North Africa, Panama)",
+    -12.046374,
+    -77.042793
+  ],
+  [
+    "SEED4NA - SDI & EO Training & Education for North Africa, Spain)",
+    -12.046374,
+    -77.042793
+  ],
+  [
+    "SEED4NA - SDI & EO Training & Education for North Africa, Belgium (Leuven)",
+    -12.046374,
+    -77.042793
+  ],
+  [
+    "SEED4NA - SDI & EO Training & Education for North Africa, Austria (Graz)",
+    -12.046374,
+    -77.042793
+  ],
+  [
+    "SEED4NA - SDI & EO Training & Education for North Africa, Spain (Zaragoza)",
+    -12.046374,
+    -77.042793
+  ],
+  [
+    "SEED4NA - SDI & EO Training & Education for North Africa, Mozambique)",
+    -12.046374,
+    -77.042793
+  ],
+  [
+    "SEED4NA - SDI & EO Training & Education for North Africa, Italy)",
+    -12.046374,
+    -77.042793
+  ],
+  [
+    "SEED4NA - SDI & EO Training & Education for North Africa, Italy)",
+    -12.046374,
+    -77.042793
+  ],
+  [
+    "SEED4NA - SDI & EO Training & Education for North Africa, Spain",
     -12.046374,
     -77.042793
   ],
